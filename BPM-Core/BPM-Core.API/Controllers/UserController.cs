5ff4044e9aa97108a427dde859d13f755bf8f045
Nazar Yavorskyi <nazar.yavorskiy26@gmail.com>
2024-05-20 21:03:37 +0300

    Added part "API" to name of project BPM-Core

--- BPM-Core/BPM-Core.API/Controllers/UserController.cs
+++ BPM-Core/BPM-Core.API/Controllers/UserController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BPM_Core.Controllers
+namespace BPM_Core.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
